0a6fc7003252d61ff16a2141de19453b23f0a2b0
qianbimo <qianbimozi@gmail.com>
2022-07-28 12:10:44 +0800

    测试代码 DAY1-DAY4

---
+++ lesson_1_1/lesson_1_1/test_1_1.c
@@ -0,0 +1,79 @@
+
+#include <stdio.h>
+#include <string.h>
+// 
+//
+//int main()
+//{
+//	printf("hello wrold \n");
+//		return 0;
+//}
+//int main()
+//{
+//    printf("%d\n", sizeof(char));
+//    printf("%d\n", sizeof(short));
+//    printf("%d\n", sizeof(int));
+//    printf("%d\n", sizeof(long));
+//    printf("%d\n", sizeof(long long));
+//    printf("%d\n", sizeof(float));
+//    printf("%d\n", sizeof(double));
+//    
+//    return 0;
+//}
+//int a = 100;
+//int main()
+//{
+//    //局部变量 - {}内部定义的变量
+//    int a = 10;
+//    printf("%d\n", a);
+//
+//    return 0;
+//}
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int sum = 0;
+//    scanf("%d %d", &a, & b);
+//    sum = a + b;
+//    printf("%d", sum);
+//    return 0;
+//}
+//int main()
+//{
+//	printf("     **\n     **\n************\n************\n    *  *\n    *  *\n");
+//	return 0;
+//}
+//int main()
+//{
+//    printf("1:%d\n", global);
+//    printf("mozi\n");
+//    {
+//        int a = 10;
+//        printf("a=%d\n", a);
+//        printf("2:%d\n", global);
+//    }
+//    printf("3:%d\n", global);
+//    return 0;
+//}
+//int main()
+//{
+//    char arr[] = "abcde";//[]不填写，会自动根据后续内容进行初始化
+//    return 0;
+//}
+//int main()
+//{
+//    char arr1[] = "abc";
+//    char arr2[] = { 'a','b','c' };
+//
+//    printf("%s\n", arr1);//%s是打印字符串用的
+//    printf("%s\n", arr2);
+//    return 0;
+//}
+int main()
+{
+    char arr2[] = { 'a','b','c' };
+
+    printf("%d\n", arr2);
+    return 0;
+}
---
+++ lesson_1_1/lesson_1_1/test_1_2.c
@@ -0,0 +1,25 @@
+#define CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int mian()
+//{
+//#define MAX 10000
+//    int M = MAX;
+//    printf("M = %d\n", M);
+//    return 0;
+////}
+//enum SEX  //在这里可以列举出枚举变量未来的可能取值
+//{
+//    MALE = 3,//可以在这里修改
+//    FEMALE,
+//    SECRET
+//};
+//int main()
+//{
+//    enum SEX s = MALE;
+//    //MALE = 3;//err 不能在这里修改
+//    printf("%d\n", MALE);
+//    printf("%d\n", FEMALE);
+//    printf("%d\n", SECRET);
+//    return 0;
+//}
